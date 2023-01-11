fe78dc1f8d15349d301bbf80c0f3ff2077bba5bd
Saxolee Technology Services <118402978+saxolee-tech@users.noreply.github.com>
2023-01-11 21:36:53 +0000

    Update  DevOps Integration Application
    Update  DevOps Integration Application

--- src/main/java/com/javatechie/DevopsIntegrationApplication.java
+++ src/main/java/com/javatechie/DevopsIntegrationApplication.java
@@ -1,4 +1,4 @@
-package com.javatechie;
+package com.saxolee tech services;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,7 +11,7 @@ public class DevopsIntegrationApplication {
 
 	@GetMapping
 	public String message(){
-		return "welcome to javatechie";
+		return "welcome to saxolee tech services";
 	}
 
 	public static void main(String[] args) {
